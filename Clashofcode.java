98f7ed293b49591fc504f2fad971550b37ee50ee
Pattiya Yiadram <100212611+PattiyaY@users.noreply.github.com>
2023-01-28 19:57:18 +0700

    Clash of code

---
+++ Clashofcode.java
@@ -0,0 +1,34 @@
+package exercises;
+import java.util.*;
+public class Clashofcode {
+
+	public static void main(String[] args) {
+
+		        Scanner in = new Scanner(System.in);
+		        int result = 0;
+		        String[] x = new String[18];
+		        String[] y = new String[18];
+		        System.out.println("For X");
+		        for (int i = 0; i < 18; i++) {
+		            x[i] = in.next();
+		        }
+		        System.out.println("For Y");
+		        for (int i = 0; i < 18; i++) {
+		            y[i] = in.next();
+		        }
+
+		        for(int j = 0; j < 18; j++){
+		            result += Integer.parseInt(y[j]);
+		            result -= Integer.parseInt(x[j]);
+		        }
+		        System.out.println("The result is : " + result);
+		        in.close();
+		        
+		        /* Plus the number of Y and subtract by the number of X go one by one 
+		         *  until the last one 
+		         *  For example : x = 1 2 3 4 5 6 7 8 9 0 9 8 7 6 5 4 3 2 
+		         *  			  y = 0 9 8 7 6 5 4 3 2 1 1 2 3 4 5 6 7 8
+		         *  			  result = -8 
+		         */
+	}
+}
---
+++ Fx_formular.java
@@ -0,0 +1,41 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Fx_formular {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+        int b = in.nextInt();
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+            arr[i] += x;
+        }
+        for(int i = 0; i < n-1; i++) {
+            if(arr[i] != arr[i+1]){
+                System.out.print(a*arr[i]+b + " "); 
+            } else {
+                continue;
+            }
+        }
+        System.out.print(a*arr[arr.length-1]+b+ " ");
+        
+			/* a = 5, b = -2, amount of number = 3, numbers = 3 8 -1 
+        	 * (formula : F(x) = a*x+b) 
+        	 * input : 5
+        	 * 		   -2
+        	 * 		   3
+        	 * 		   3
+        	 * 		   8
+        	 * 		   -1
+        	 * output : 13 38 -7
+        	 */
+        in.close();
+
+	}
+
+}
