763f8cfb0700df5e0997904b1a2324c914d3891c
Pattiya Yiadram <100212611+PattiyaY@users.noreply.github.com>
2023-01-28 19:58:16 +0700

    Update Clashofcode.java

--- Clashofcode.java
+++ Clashofcode.java
@@ -26,8 +26,8 @@ public class Clashofcode {
 		        /* Plus the number of Y and subtract by the number of X go one by one 
 		         *  until the last one 
 		         *  For example : x = 1 2 3 4 5 6 7 8 9 0 9 8 7 6 5 4 3 2 
-		         *  			  y = 0 9 8 7 6 5 4 3 2 1 1 2 3 4 5 6 7 8
-		         *  			  result = -8 
+		         *  		  y = 0 9 8 7 6 5 4 3 2 1 1 2 3 4 5 6 7 8
+		         *  	  	  result = -8 
 		         */
 	}
 }
