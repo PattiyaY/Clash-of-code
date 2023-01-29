50762bbe6984fadafc75f68344ccaf5d32fdb63d
Pattiya Yiadram <100212611+PattiyaY@users.noreply.github.com>
2023-01-29 17:57:17 +0700

    Create Total people in the town.py

---
+++ Total people in the town.py
@@ -0,0 +1,14 @@
+x = int(input())
+print(x**3 + x**2 + x)
+
+# "x" men have "x" wives and "x" wives have "x" kids 
+#print the total of people in the town
+
+#input : 7 
+#output : 399
+
+#input : 2
+#output : 14
+
+#input : 0
+#output : 0
