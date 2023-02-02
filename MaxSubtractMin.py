4d300690873204eadd43acdfb87a026f635693ab
Pattiya Yiadram <100212611+PattiyaY@users.noreply.github.com>
2023-02-03 00:01:03 +0700

    Create MaxSubtractMin.py

---
+++ MaxSubtractMin.py
@@ -0,0 +1,13 @@
+#Find the result of maximum number subtract by minimum number
+a = []
+for i in range(4):
+  a.append(int(input()))
+print(max(a)-min(a))
+
+#----- For example -----
+#input : 1
+#        10
+#        100
+#        1
+
+#output : 99
